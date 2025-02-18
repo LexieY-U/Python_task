82d079da89831fbe2f45de0bca75290ab7e252f6
Jiyin Yu <jiyin.yu@accenture.com>
2025-02-18 16:29:00 +0000

    Create a home page for my bookshop

--- my-fastapi-app/app/main.py
+++ my-fastapi-app/app/main.py
@@ -7,9 +7,6 @@ app = FastAPI()
 # Define a GET endpoint that will return a simple welcome message. 
 # Use the @app.get decorator to specify the route and the HTTP method.
 @app.get("/") # this endpoint will respond to GET requests at the root URL path (/)
-async def read_root(): # defines an asynchronous function named read_root that will handle incoming requests to this endpoint.
-    return {"message": "Hello to my first FastAPI endpoint!"} # returns a JSON response
+async def home(): # defines an asynchronous function named read_root that will handle incoming requests to this endpoint.
+    return {"message": "Welcome to my bookshop!"} # returns a JSON response
 
-@app.get("/get-current-time")
-async def get_current_time():
-    return {"The current time is 3pm."}
