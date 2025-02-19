90fdbc80606ed7fd25b24ef905f02ef86a813d05
Jiyin Yu <jiyin.yu@accenture.com>
2025-02-19 22:48:31 +0000

    Updated the autoincremented id function and moved the base to database

--- my-fastapi-app/app/models.py
+++ my-fastapi-app/app/models.py
@@ -1,15 +1,14 @@
 # SQLAlchemy ORM models creation
 
 from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
+from database import Base
 
-Base = declarative_base()
 # Create a base class for ORM models - every model will inherit from this class.
 class Book(Base):
     # Set the table name to "books" - SQLAlchemy will create a table with this name in the database.
     __tablename__ = "books"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String, index=True, nullable=False) # Cannot be empty (required field)
     author = Column(String, index=True, nullable=False)
     price = Column(Float, nullable=False)
