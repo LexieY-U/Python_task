fcefae547c71856b3de65e5b2f7aed94f45e1c4e
Jiyin Yu <jiyin.yu@accenture.com>
2025-02-19 15:44:25 +0000

    Add get and delete for main.py

--- my-fastapi-app/app/main.py
+++ my-fastapi-app/app/main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+# FastAPI app entry point
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import engine, Base, get_db
+from models import Book
+from schemas import BookCreate, BookResponse
+from curd import create_book, get_books, delete_book
 
 # Import the FastAPI class from the fastapi package and initialize a new FastAPI instance.
 app = FastAPI()
@@ -10,6 +17,19 @@ app = FastAPI()
 async def home(): # defines an asynchronous function named read_root that will handle incoming requests to this endpoint.
     return {"message": "Welcome to my bookshop!"} # returns a JSON response
 
-@app.get("/list-of-books")
-async def list_of_books():
-    return []
+
+@app.post("/books/", response_model=BookResponse)
+def add_book(book: BookCreate, db: Session = Depends(get_db)):
+    db_book = create_book(db, book)    
+    return db_book
+
+@app.get("/books/", response_model=list[BookResponse])
+def list_books(db: Session = Depends(get_db)):
+    return get_books(db)
+
+@app.delete("/books/{book_id}")
+def remove_book(book_id: int, db: Session = Depends(get_db)):
+    book = delete_book(db, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return {"message": "Book deleted successfully"}
