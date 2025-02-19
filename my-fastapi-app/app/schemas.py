d421625c92402ae02c109010f84c5afdb17fdfcf
Jiyin Yu <jiyin.yu@accenture.com>
2025-02-19 08:56:12 +0000

    Made some changes regarding the comment for orm_mode

--- my-fastapi-app/app/schemas.py
+++ my-fastapi-app/app/schemas.py
@@ -2,15 +2,18 @@ from pydantic import BaseModel, Field
 
 # Create book schema
 class BookCreate(BaseModel):
+    id: int
     title: str = Field(..., min_length=1, max_length=100) # Field Validation
     author: str = Field(..., min_length=1, max_length=50)
     price: float = Field(..., gt=0)  # Price must be > 0; gt -> greater than
     stock: int = Field(..., ge=0)    # Stock must be >= 0 -> greater and equal than
 
+# Create response format when retrieving books from the database
+# Inherits all fields from BookCreate, plus an ID
 class BookResponse(BookCreate):
     id: int
 
     # Customise model behavior using the Config class
     class Config:
-        # This allows the model to work with ORMs by reading data from attributes instead of dict keys
+        # Enable automatic conversion from database models to Pydantic models.
         orm_mode = True
